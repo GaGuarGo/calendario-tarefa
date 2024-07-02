3166659dcb6aa0dbb68cbf394969dd832c952a76
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-07-02 12:34:45 -0300

    Criação dos Models e Relaçaõ

---
+++ database/migrations/2024_07_02_153110_create_tarefas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tarefas', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('titulo');
+            $table->string('descricao')->nullable();
+            $table->date('prazo')->nullable()->default(null);
+            $table->boolean('status')->default(false);
+            $table->foreignIdFor(User::class)->constrained();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tarefas');
+    }
+};
